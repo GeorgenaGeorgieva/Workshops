b85e9d0e82f7dd25e0afcd3652ab9895f7298493
Georgena Georgieva <57866154+GeorgenaGeorgieva@users.noreply.github.com>
2019-12-17 17:34:40 +0200

    C#

---
+++ Linked List/CoolStack.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Linked_List
+{
+    public class CoolStack
+    {
+        private List<object> values;
+
+        public CoolStack()
+        {
+            this.values = new List<object>();
+        }
+
+        public int Count
+        {
+            get
+            {
+                return this.values.Count;
+            }
+        }
+
+        public void Push(object value)
+        {
+            this.values.Add(value);
+        }
+
+        public object Pop()
+        {
+            if (values.Count == 0)
+            {
+                throw new InvalidOperationException("Cool stack is emty!");
+            }
+
+            var lastIndex = this.values.Count - 1;
+            var last = this.values[lastIndex];
+            this.values.RemoveAt(lastIndex);
+            return last;
+        }
+
+        public void ForEach(Action<object> action)
+        {
+            foreach (var item in values)
+            {
+                action(item);
+            }
+        }
+    }
+}
