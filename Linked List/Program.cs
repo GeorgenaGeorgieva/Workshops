fb81ca6a751c3d9acf21ee160cb3b7fd075b2a15
Georgena Georgieva <georgena1991@gmail.com>
2020-03-17 22:53:09 +0200

    Update Program.cs

--- Linked List/Program.cs
+++ Linked List/Program.cs
@@ -1,8 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-
-namespace Linked_List
+namespace LinkedList
 {
+    using System;
+    using System.Collections.Generic;
+    
     public class Program
     {
        public static void Main()
